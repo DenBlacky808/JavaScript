464d11118602fc9119e7f10247ba48c964b3e28b
DenisChernyavskiy <92929@mail.ru>
2021-06-07 19:45:58 +0300

    homeWork2

---
+++ homeWork2/task5.js
@@ -0,0 +1,28 @@
+/*
+5. Реализовать основные 4 арифметические операции
+в виде функций с двумя параметрами.
+Обязательно использовать оператор return.
+*/
+
+function sumFunc(a, b) {
+    return a + b
+}
+
+function diffFunc(a, b) {
+    return a - b
+}
+
+function multFunc(a, b) {
+    return a * b
+}
+
+function divFunc(a, b) {
+    return a / b
+}
+
+console.log (
+    sumFunc(5, 2),
+    diffFunc(5, 2),
+    multFunc(5, 2),
+    divFunc(5, 2)
+)
